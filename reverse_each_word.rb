24651e7d706437f8cd412b008024b3c3abd61375
Hanna Mulugeta <hannabmulugeta@gmail.com>
2021-01-06 19:04:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@
 def reverse_each_word(string)
   array = []
   sentence = string.split
-  sentence.map{|word| array << word.reverse}
+  sentence.collect {|word| array << word.reverse}
   array.join(" ")
 end
